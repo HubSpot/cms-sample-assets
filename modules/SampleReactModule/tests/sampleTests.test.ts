d927048ffce2fb2566b76648337443287d4efdf6
Brandon Van Dyck <bvandyck@hubspot.com>
2024-02-27 14:44:05 -0500

    Rename instances of JSR to React

--- modules/SampleReactModule/tests/sampleTests.test.ts
+++ modules/SampleReactModule/tests/sampleTests.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { addNumbers } from '../InteractiveComponents/index.js';
 
 // An example of what unit tests might look like with modules
-describe('SampleJSR', () => {
+describe('SampleReactModule', () => {
   describe('addTest', () => {
     it('will return itself if it only has one argument', () => {
       expect(addNumbers(1)).toEqual(1);
